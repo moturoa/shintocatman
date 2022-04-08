75c1968f4355fe60c9a208611adb97ca2d049b8a
Remko Duursma <remko@shintolabs.nl>
2022-04-08 15:38:12 +0200

    jsonedit in text

--- R/jsonEditModule.R
+++ R/jsonEditModule.R
@@ -5,39 +5,25 @@
 #' @param input
 #' @rdname jsonEdit
 #' @export
-jsonEditModuleUI <- function(id, class = "", 
-                             options = c("delete","add"),
-                             options_labels = c(delete = "Laatste verwijderen", add = "Toevoegen", save = "Opslaan")
-                             ){
+jsonEditModuleUI <- function(id, class = "", icon = NULL, label = "Edit values"){
   
   ns <- NS(id)
   
-  tags$div(class = class,
-    
-      uiOutput(ns("ui_edit")),
-    
-      if("add" %in% options){
-        actionButton(ns("btn_add_cat"), options_labels[["add"]], 
-                             icon = softui:::bsicon("plus"))
-      },
-      if("delete" %in% options){
-        actionButton(ns("btn_del_cat"), options_labels[["delete"]], 
-                     icon = softui::bsicon("minus"), class = "btn-warning")  
-      },
-      
-      actionButton(ns("btn_save"), options_labels[["save"]], 
-                     class = "btn-success",
-                     icon = softui::bsicon("save-fill"))
-  )
+  actionButton(ns("btn"),label, icon = icon, class = class)
   
 }
 
 #' @rdname jsonEdit
 #' @export
 jsonEditModule <- function(input, output, session, 
-                           edit = c("key","value"),
+                           options = reactive(c("delete","add")),
+                           options_labels = c(delete = "Laatste verwijderen", 
+                                              add = "Toevoegen", 
+                                              save = "Opslaan"),
+                           edit = reactive(c("key","value")),
                            widths = c(6,6),
-                           value = reactive(NULL)){
+                           value = reactive(NULL),
+                           callback = function(data){}){
   
   
   value_txt <- reactive({
@@ -48,7 +34,36 @@ jsonEditModule <- function(input, output, session,
   observeEvent(value_txt(),{
     n_cat(length(value_txt()))
   })
+
   
+  observeEvent(input$btn, {
+    
+    
+    showModal(
+      modalDialog(
+        
+        tags$div(
+          uiOutput(session$ns("ui_edit")),
+                 
+                 if("add" %in% options()){
+                   actionButton(session$ns("btn_add_cat"), options_labels[["add"]], 
+                                icon = softui:::bsicon("plus"))
+                 },
+                 if("delete" %in% options()){
+                   actionButton(session$ns("btn_del_cat"), options_labels[["delete"]], 
+                                icon = softui::bsicon("minus"), class = "btn-warning")  
+                 },
+                 
+                 actionButton(session$ns("btn_save"), options_labels[["save"]], 
+                              class = "btn-success",
+                              icon = softui::bsicon("save-fill"))
+        )
+      )
+    )
+    
+  })
+  
+    
   output$ui_edit <- renderUI({
     
     val <- value_txt()
@@ -58,20 +73,20 @@ jsonEditModule <- function(input, output, session,
     lapply(1:n, function(i){
      
       key <- ifelse(i > length(val), "", names(val)[i])
-      if(key == "" & !("key" %in% edit)){
+      if(key == "" & !("key" %in% edit())){
         key <- as.character(i)
       }
       key_edit <- textInput(session$ns(paste0("key_",i)), NULL, value = key, width = "100%")
-      if(!("key" %in% edit)){
+      if(!("key" %in% edit())){
         key_edit <- shinyjs::disabled(key_edit)
       }
       
       val <- ifelse(i > length(val), "", val[[i]])
-      if(val == "" & !("value" %in% edit)){
+      if(val == "" & !("value" %in% edit())){
         val <- as.character(i)
       }
       val_edit <- textInput(session$ns(paste0("val_",i)), NULL, value = val, width = "100%")
-      if(!("value" %in% edit)){
+      if(!("value" %in% edit())){
         val_edit <- shinyjs::disabled(val_edit)
       }
     
@@ -101,16 +116,17 @@ jsonEditModule <- function(input, output, session,
   
   observeEvent(input$btn_save, {
     
+    
     val <- value_txt()
     n <- n_cat()
     req(n>0)
-    if("value" %in% edit){
+    if("value" %in% edit()){
       for(i in 1:n){
         val[[i]] <- input[[paste0("val_",i)]]   
       }  
     }
     
-    if("key" %in% edit){
+    if("key" %in% edit()){
       for(i in 1:n){
         names(val)[i] <- input[[paste0("key_",i)]]   
       }  
@@ -120,6 +136,8 @@ jsonEditModule <- function(input, output, session,
     
     txt_out(jsonlite::toJSON(val, auto_unbox = TRUE))
     
+    removeModal()
+    callback()
   })
   
 return(txt_out)
@@ -134,7 +152,7 @@ test_jsonedit <- function(){
   
   ui <- fluidPage(style = "margin: auto; width: 600px;",
                   useShinyjs(),
-      actionButton("btn_edit", "Edit value"),
+                  jsonEditModuleUI("test"),
       verbatimTextOutput("txt_out")
       
   )
@@ -142,25 +160,13 @@ test_jsonedit <- function(){
   server <- function(input, output, session) {
     
     
-    observeEvent(input$btn_edit, {
-      showModal(
-        modalDialog(
-          title = "Edit",
-          jsonEditModuleUI("test", options = c("add","delete")),
-          footer = tagAppendAttributes(modalButton("Cancel"), class = "btn-danger")
-        )
-      )
-    })
-    
     edited_txt <- callModule(jsonEditModule, "test", 
-                             edit = "value",
+                             options = reactive(c("add","delete")),
+                             edit = reactive("value"),
                              widths = c(2,10),
                              value = reactive("{\"1\":\"\",\"2\":\"Gereed\",\"3\":\"In aanbouw genomen\",\"4\":\"Intake\",\"5\":\"Onherroepelijk plan/besluit\",\"6\":\"Ontwerpplan/besluit\",\"7\":\"Planologische voorbereiding\",\"8\":\"Potentieel\",\"9\":\"Start Project\",\"10\":\"Vastgesteld plan/besluit\",\"11\":\"Vergunning verleend\"}"))
    
-    observeEvent(edited_txt(), {
-      removeModal()
-    })
-    
+
     output$txt_out <- renderPrint({
       edited_txt()
     }) 
