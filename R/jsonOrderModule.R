c5441e63976775f816f14e120d9bf9e49d729f42
Remko Duursma <remko@shintolabs.nl>
2022-04-08 15:28:21 +0200

    button

--- R/jsonOrderModule.R
+++ R/jsonOrderModule.R
@@ -6,11 +6,11 @@
 #' @param input
 #' @rdname jsonOrder
 #' @export
-jsonOrderModuleUI <- function(id){
+jsonOrderModuleUI <- function(id, label = "Change order", icon = NULL, class = ""){
   
   ns <- NS(id)
   
-  uiOutput(ns("input_field"))
+  actionButton(ns("btn"),label, icon = icon, class = class)
   
 }
 
@@ -18,34 +18,66 @@ jsonOrderModuleUI <- function(id){
 
 #' @rdname jsonOrder
 jsonOrderModule <- function(input, output, session,
-                            data = NULL,
-                            order_column = NULL,
-                            label_column = NULL
+                            data = reactive(NULL),
+                            order_column = reactive(NULL),
+                            label_column = reactive(NULL),
+                            callback = function(data){}
                             ){
   
-  order_data <- from_json(data[[order_column]])
-  labels <- from_json(data[[label_column]])
+  order_data <- reactive({
+    req(order_column())
+    from_json(data()[[order_column()]])  
+  })
   
-  output$input_field <- renderUI({
-    rank_list(
-      text = "Change the order",
-      labels = labels,
-      input_id = session$ns("value")
-    )  
+  labels <- reactive({
+    req(order_data())
+    if(is.null(label_column())){
+      as.character(seq(1, length(order_data())))
+    } else {
+      from_json(data()[[label_column()]])
+    }
+  })
+    
+  observeEvent(input$btn, {
+    
+    showModal(
+      modalDialog(
+        title = "Drag to change the order",
+        rank_list(
+          text = "",
+          labels = labels(),
+          input_id = session$ns("value")
+        ),
+        footer = tagList(actionButton(session$ns("btn_confirm"),
+                                      "Opslaan",
+                                      icon = icon("check"),
+                                      class = "btn-success"),
+                         modalButton("Annuleren")
+        )
+      )
+    )
+    
+    
   })
   
   order_int <- reactive({
-    match(input$value, names(labels))
+    match(input$value, names(labels()))
   })
   
+  output_vector <- reactiveVal()
   
-  output_vector <- reactive({
+  observeEvent(input$btn_confirm, {
+    
+    output_vector(
+      to_json(as.list(order_int()))  
+    )
+    
+    callback()
+    removeModal()
+    
+  })
+  
 
-    req(order_int())
-    to_json(as.list(order_int()))
-   
-  })
-  
 return(output_vector)
 }
 
@@ -65,12 +97,14 @@ test_jsonOrderModule <- function(){
     
     out <- callModule(jsonOrderModule, "test",
                
-               data = tibble(
-                 labels = to_json(setNames(list("aap","banaan","tak","boom"),as.character(1:4))),
-                 order = to_json(setNames(as.list(1:4),1:4))
-               ),
-               order_column = "order",
-               label_column = "labels"
+               data = reactive({
+                 tibble(
+                  labels = to_json(setNames(list("aap","banaan","tak","boom"),as.character(1:4))),
+                  order = to_json(setNames(as.list(1:4),1:4))
+                )
+               }),
+               order_column = reactive("order"),
+               label_column = reactive("labels")
                )
     
     output$out <- renderPrint({
