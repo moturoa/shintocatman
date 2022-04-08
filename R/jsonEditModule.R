722fff46e8f02245fe7413e692526f29e60c0032
Remko Duursma <remko@shintolabs.nl>
2022-04-08 13:06:58 +0200

    jsonEdit

--- R/jsonEditModule.R
+++ R/jsonEditModule.R
@@ -1,7 +1,13 @@
-
-
+#' Shiny JSON edit module
+#' @param class CSS class of the editor
+#' @param opties "verwijderen", "toevoegen", or both
+#' @param input
+#' @rdname jsonEdit
+#' @export
 jsonEditModuleUI <- function(id, class = "", 
-                             opties = c("verwijderen","toevoegen")){
+                             options = c("delete","add"),
+                             options_labels = c(delete = "Laatste verwijderen", add = "Toevoegen", save = "Opslaan")
+                             ){
   
   ns <- NS(id)
   
@@ -9,22 +15,28 @@ jsonEditModuleUI <- function(id, class = "",
     
       uiOutput(ns("ui_edit")),
     
-      actionButton(ns("btn_add_cat"), "Toevoegen", 
-                             icon = softui:::bsicon("plus")),
-      if("verwijderen" %in% opties){
-        actionButton(ns("btn_del_cat"), "Laatste verwijderen", 
+      if("add" %in% options){
+        actionButton(ns("btn_add_cat"), options_labels[["add"]], 
+                             icon = softui:::bsicon("plus"))
+      },
+      if("delete" %in% options){
+        actionButton(ns("btn_del_cat"), options_labels[["delete"]], 
                      icon = softui::bsicon("minus"), class = "btn-warning")  
       },
-      if("toevoegen" %in% opties){
-        actionButton(ns("btn_save"), "Opslaan", 
+      
+      actionButton(ns("btn_save"), options_labels[["save"]], 
                      class = "btn-success",
-                     icon = softui::bsicon("save-fill"))  
-      }
+                     icon = softui::bsicon("save-fill"))
   )
   
 }
 
-jsonEditModule <- function(input, output, session, value = reactive(NULL)){
+#' @rdname jsonEdit
+#' @export
+jsonEditModule <- function(input, output, session, 
+                           edit = c("key","value"),
+                           widths = c(6,6),
+                           value = reactive(NULL)){
   
   
   value_txt <- reactive({
@@ -44,11 +56,27 @@ jsonEditModule <- function(input, output, session, value = reactive(NULL)){
     
     lapply(1:n, function(i){
      
-      val <- ifelse(i > length(val), "", val[[i]])
+      key <- ifelse(i > length(val), "", names(val)[i])
+      if(key == "" & !("key" %in% edit)){
+        key <- as.character(i)
+      }
+      key_edit <- textInput(session$ns(paste0("key_",i)), NULL, value = key, width = "100%")
+      if(!("key" %in% edit)){
+        key_edit <- shinyjs::disabled(key_edit)
+      }
       
-      fluidRow(
-        column(3, i),
-        column(9, textInput(session$ns(paste0("txt_",i)), NULL, value = val))
+      val <- ifelse(i > length(val), "", val[[i]])
+      if(val == "" & !("value" %in% edit)){
+        val <- as.character(i)
+      }
+      val_edit <- textInput(session$ns(paste0("val_",i)), NULL, value = val, width = "100%")
+      if(!("value" %in% edit)){
+        val_edit <- shinyjs::disabled(val_edit)
+      }
+    
+      softui::fluid_row(
+        column(widths[1], key_edit),
+        column(widths[2], val_edit)
       )
       
     })
@@ -75,10 +103,19 @@ jsonEditModule <- function(input, output, session, value = reactive(NULL)){
     val <- value_txt()
     n <- n_cat()
     req(n>0)
-    for(i in 1:n){
-      val[[i]] <- input[[paste0("txt_",i)]]   
+    if("value" %in% edit){
+      for(i in 1:n){
+        val[[i]] <- input[[paste0("val_",i)]]   
+      }  
     }
-    val <- val[1:n_cat()]
+    
+    if("key" %in% edit){
+      for(i in 1:n){
+        names(val)[i] <- input[[paste0("key_",i)]]   
+      }  
+    }
+    
+    val <- val[1:n_cat()]  # is dit nodig?
     
     txt_out(jsonlite::toJSON(val, auto_unbox = TRUE))
     
@@ -95,10 +132,7 @@ test_jsonedit <- function(){
   library(shiny)
   
   ui <- fluidPage(style = "margin: auto; width: 600px;",
-                  
-                  inlineCSS("
-                            .mylist{border: 1px solid hotpink;}
-                            "),
+                  useShinyjs(),
       actionButton("btn_edit", "Edit value"),
       verbatimTextOutput("txt_out")
       
@@ -111,13 +145,15 @@ test_jsonedit <- function(){
       showModal(
         modalDialog(
           title = "Edit",
-          jsonEditModuleUI("test", class = "mylist", opties = "toevoegen"),
+          jsonEditModuleUI("test", options = c("add","delete")),
           footer = tagAppendAttributes(modalButton("Cancel"), class = "btn-danger")
         )
       )
     })
     
     edited_txt <- callModule(jsonEditModule, "test", 
+                             edit = "value",
+                             widths = c(2,10),
                              value = reactive("{\"1\":\"\",\"2\":\"Gereed\",\"3\":\"In aanbouw genomen\",\"4\":\"Intake\",\"5\":\"Onherroepelijk plan/besluit\",\"6\":\"Ontwerpplan/besluit\",\"7\":\"Planologische voorbereiding\",\"8\":\"Potentieel\",\"9\":\"Start Project\",\"10\":\"Vastgesteld plan/besluit\",\"11\":\"Vergunning verleend\"}"))
    
     observeEvent(edited_txt(), {
