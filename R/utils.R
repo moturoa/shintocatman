d1cb35fc9c33d8a112cdc3ab99a6f5d48ab96d8e
Remko Duursma <remko@shintolabs.nl>
2022-04-11 12:00:55 +0200

    safe json

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,18 @@
 
 #' Json Utilities
+#' @description Wrapper around `jsonlite::fromJSON`, returns input argument if it
+#' cannot be converted from JSON. 
 #' @export
 #' @rdname json
 from_json <- function(x, ...){
-  jsonlite::fromJSON(x, ...)
+  
+  if(!is.character(x))return(x)
+  
+  tryCatch(
+    jsonlite::fromJSON(x, ...),
+    error = function(e)x
+  )
+  
 }
 
 #' @rdname json
