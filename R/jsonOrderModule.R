a3218ab098994ff47f65272e49d41d8e2fdbdaba
Remko Duursma <remko@shintolabs.nl>
2022-07-18 13:34:17 +0200

    jsonordermodule not in modal

--- R/jsonOrderModule.R
+++ R/jsonOrderModule.R
@@ -1,16 +1,13 @@
 
 #' Shiny JSON order edit module
-#' @description Editor for ordering a JSON vector e.g. [1,2,3,4]
-#' @param class CSS class of the editor
-#' @param opties "verwijderen", "toevoegen", or both
-#' @param input
+#' @description Editor for ordering a JSON vector e.g. '[1,2,3,4]'
 #' @rdname jsonOrder
 #' @export
-jsonOrderModuleUI <- function(id, label = "Change order", icon = NULL, status = "secondary"){
+jsonOrderModuleUI <- function(id){
   
   ns <- NS(id)
   
-  softui::action_button(ns("btn"), label, icon = icon, status = status)
+  uiOutput(ns("ui_rank_list"))
   
 }
 
@@ -22,11 +19,17 @@ jsonOrderModuleUI <- function(id, label = "Change order", icon = NULL, status = 
 jsonOrderModule <- function(input, output, session,
                             data = reactive(NULL),
                             order_column = reactive(NULL),
-                            label_column = reactive(NULL),
-                            title = "Drag to change the order",
-                            header_ui = NULL,
-                            callback = function(data){}
+                            label_column = reactive(NULL)
                             ){
+  
+  
+  output$ui_rank_list <- renderUI({
+    sortable::rank_list(
+      text = "",
+      labels = labels(),
+      input_id = session$ns("value")
+    )
+  })
   
   order_data <- reactive({
     from_json(data()[[order_column()]])  
@@ -40,43 +43,17 @@ jsonOrderModule <- function(input, output, session,
       from_json(data()[[label_column()]])[order_data()]
     }
   })
-    
-  observeEvent(input$btn, {
-    
-    showModal(
-      softui::modal(
-        title = title,
-        id_confirm = "btn_confirm", close_txt = "Annuleren",
-        
-        header_ui,
-        
-        sortable::rank_list(
-          text = "",
-          labels = labels(),
-          input_id = session$ns("value")
-        )
-        )
-      )
-    
-  })
-  
+
   order_int <- reactive({
     match(input$value, names(labels()))
   })
   
-  output_vector <- reactiveVal()
-  
-  observeEvent(input$btn_confirm, {
-    
-    out <- to_json(as.list(order_int()))  
-    callback(out)
-    
-    output_vector(out)
-    
+  out <- reactive({
+    to_json(as.list(order_int()))  
   })
   
 
-return(output_vector)
+return(out)
 }
 
 
@@ -85,10 +62,13 @@ return(output_vector)
 test_jsonOrderModule <- function(){
   library(shiny)
   
-  ui <- fluidPage(style = "margin: auto; width: 600px;",
+  ui <- softui::simple_page(style = "margin: auto; width: 600px;",
     jsonOrderModuleUI("test"),
     tags$hr(),
-    verbatimTextOutput("out")
+    verbatimTextOutput("out"),
+    tags$hr(),
+    softui::action_button("btn_go_modal", "in modal", status = "success")
+    
   )
   
   server <- function(input, output, session) {
@@ -108,6 +88,22 @@ test_jsonOrderModule <- function(){
     output$out <- renderPrint({
       out()
     })
+    
+    softui::modalize(trigger_open = reactive(input$btn_go_modal),
+                     ui_module = jsonOrderModuleUI,
+                     server_module = jsonOrderModule,
+                     server_pars = list(
+                     data = reactive({
+                       tibble(
+                        labels = to_json(setNames(list("aap","banaan","tak","boom"),as.character(1:4))),
+                        order = to_json(c(4,1,2,3))
+                       )
+                     }),
+                     order_column = reactive("order"),
+                     label_column = reactive("labels")
+       )
+    )    
+    
     
   }
   
