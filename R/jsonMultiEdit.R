6831b2e92b004f716ff3c54419d6d53e73b15545
Remko Duursma <remko@shintolabs.nl>
2022-10-10 14:23:09 +0200

    multiedit bug fixes

--- R/jsonMultiEdit.R
+++ R/jsonMultiEdit.R
@@ -8,17 +8,16 @@
 #' @export
 jsonMultiEditUI <- function(id, 
                             ui_header = NULL,
-                            select_label = "Categorie",
                             choices_label = "Keuzelijst"){
   
   ns <- NS(id)
   
   tags$div(
     ui_header,
-    selectInput(ns("sel_key"), select_label, choices = NULL),
+    uiOutput(ns("ui_key_select")),
     
     tags$label(choices_label),
-    
+     
     uiOutput(ns("ui_listeditor"))
     
   )
@@ -30,91 +29,104 @@ jsonMultiEditUI <- function(id,
 jsonMultiEdit <- function(input, output, session, 
                           edit_names = FALSE,
                           key = reactive("[]"),
-                          value = reactive("[]")){
+                          value = reactive("[]"),
+                          select_label = "Categorie",
+                          json = TRUE){
   
+
   # TODO generic convert from JSON?
   value_txt <- reactive({
+    
     req(value())
-    
-    out <- jsonlite::fromJSON(value())
-    
-    if(!is.list(value()) && stringr::str_remove_all(value(), "\"") == ""){
-      out <- jsonlite::fromJSON("[]")
+
+    out <- value()
+    if(json){
+      out <- jsonlite::fromJSON(out)
+    }
+
+    if(!is.list(out) && stringr::str_remove_all(out, "\"") == ""){
+      out <- list()
     }
     out
-    
+
   })
-  
+
   keys_data <- reactive({
-    req(value())
-    
-    jsonlite::fromJSON(key())
+    req(key())
+    out <- key()
+    if(json){
+      out <- jsonlite::fromJSON(out)
+    }
+    out
   })
-  
-  observeEvent(keys_data(), {
+
+  output$ui_key_select <- renderUI({
+
     val <- keys_data()
-    
+
+    # switch names and values
     nms <- names(val)
     val <- unname(unlist(val))
     val <- setNames(nms,val)
-    updateSelectInput(session, "sel_key", choices = val)
+    selectInput(session$ns("sel_key"), select_label, choices = val)
+
   })
-  
-  
+
+
   output$ui_listeditor <- renderUI({
-    
+
     keys <- keys_data()
-    
+
     lapply(names(keys), function(key){
-      
+
       shinyjs::hidden(
         tags$div(id = session$ns(paste0("box_",key)),
-                 listEditModuleUI(session$ns(paste0("module_",key)))    
-        )  
+          listEditModuleUI(session$ns(paste0("module_",key)))
+        )
       )
-      
+
     })
-    
+
   })
-  
+
   observeEvent(input$sel_key, {
-    
+
     key <- input$sel_key
     all_ids <- paste0("box_",names(keys_data()))
     lapply(all_ids, shinyjs::hide)
     this_id <- paste0("box_",key)
     shinyjs::show(this_id)
-    
+
   })
-  
-  
+
+
   edit_arrays <- reactive({
-    
+
     lapply(names(keys_data()), function(key){
-      
+
       vals <- value_txt()[[key]]
       names(vals) <- as.character(seq_along(vals))
-      
+
       callModule(listEditModule, paste0("module_",key),
                  data = reactive(as.list(vals)),
                  edit_name = edit_names, show_name = edit_names,
                  widths = c(6,6),
                  options = reactive(c("add","delete")),
-                 options_labels = c(delete = "Laatste verwijderen", 
+                 options_labels = c(delete = "Laatste verwijderen",
                                     add = "Toevoegen"))
-      
+
     })
-    
+
   })
-  
-  
+
+
   edit_output <- reactive({
     val <- lapply(edit_arrays(), function(x)x())
     names(val) <- names(keys_data())
     jsonlite::toJSON(val)
   })
-  
-  
+
+
   return(edit_output)
 }
 
@@ -128,7 +140,7 @@ test_jsonMultiEdit <- function(){
   ui <- softui::simple_page(
     softui::box(width=4,
                 
-          jsonMultiEditUI("test", )    
+          jsonMultiEditUI("test")    
     ),
     softui::box(width=4,
                 
@@ -146,7 +158,8 @@ test_jsonMultiEdit <- function(){
     
     out <- callModule(jsonMultiEdit, "test", 
                       key = reactive(keys),
-                      value = reactive(vals))
+                      value = reactive(vals),
+                      json = TRUE)
     
     output$txt_out <- renderPrint({
       out()
